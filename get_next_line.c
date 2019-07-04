e7b154f2dd7036e3f090ca986178df31c862e7b1
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-07-04 12:41:28 +0200

    get next line file

--- get_next_line.c
+++ get_next_line.c
@@ -6,32 +6,32 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/02 11:53:58 by xmethula          #+#    #+#             */
-/*   Updated: 2019/07/02 11:54:10 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/07/04 12:38:46 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-int		ft_new_line(char **s, char **line, int fd, int ret)
+int		get_new_line(char **s, char **line, int fd, int res)
 {
 	char	*tmp;
-	int		len;
+	int		i;
 
-	len = 0;
-	while (s[fd][len] != '\n' && s[fd][len] != '\0')
-		len++;
-	if (s[fd][len] == '\n')
+	i = 0;
+	while (s[fd][i] != '\n' && s[fd][i] != '\0')
+		i++;
+	if (s[fd][i] == '\n')
 	{
-		*line = ft_strsub(s[fd], 0, len);
-		tmp = ft_strdup(s[fd] + len + 1);
+		*line = ft_strsub(s[fd], 0, i);
+		tmp = ft_strdup(s[fd] + i + 1);
 		free(s[fd]);
 		s[fd] = tmp;
 		if (s[fd][0] == '\0')
 			ft_strdel(&s[fd]);
 	}
-	else if (s[fd][len] == '\0')
+	else if (s[fd][i] == '\0')
 	{
-		if (ret == BUFF_SIZE)
+		if (res == BUFF_SIZE)
 			return (get_next_line(fd, line));
 		*line = ft_strdup(s[fd]);
 		ft_strdel(&s[fd]);
@@ -44,13 +44,13 @@ int		get_next_line(const int fd, char **line)
 	static char	*s[255];
 	char		buf[BUFF_SIZE + 1];
 	char		*tmp;
-	int			ret;
+	int			res;
 
 	if (fd < 0 || line == NULL)
 		return (-1);
-	while ((ret = read(fd, buf, BUFF_SIZE)) > 0)
+	while ((res = read(fd, buf, BUFF_SIZE)) > 0)
 	{
-		buf[ret] = '\0';
+		buf[res] = '\0';
 		if (s[fd] == NULL)
 			s[fd] = ft_strnew(1);
 		tmp = ft_strjoin(s[fd], buf);
@@ -59,9 +59,9 @@ int		get_next_line(const int fd, char **line)
 		if (ft_strchr(buf, '\n'))
 			break ;
 	}
-	if (ret < 0)
+	if (res < 0)
 		return (-1);
-	else if (ret == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
+	else if (res == 0 && (s[fd] == NULL || s[fd][0] == '\0'))
 		return (0);
-	return (ft_new_line(s, line, fd, ret));
+	return (get_new_line(s, line, fd, res));
 }
