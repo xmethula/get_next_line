aaa326c240d4f66a9e3c43c199bca73b0c82b404
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-07-04 12:54:29 +0200

    get_next_line.h file

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: xmethula <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/27 13:14:00 by xmethula          #+#    #+#             */
-/*   Updated: 2019/07/02 11:24:17 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/07/04 12:51:43 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@
 # include <fcntl.h>
 # include "libft/libft.h"
 
-int		ft_new_line(char **s, char **line, int fd, int ret);
+int		new_line(char **s, char **line, int fd, int ret);
 int		get_next_line(const int fd, char **line);
 
 #endif
