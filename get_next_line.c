5a7f818b664246964fc99ca0ad7081df4d4b7e74
Xolani METHULA <xmethula@c4r13s2.wethinkcode.co.za>
2019-07-09 16:58:39 +0200

    get_next_line c file

--- get_next_line.c
+++ get_next_line.c
@@ -6,58 +6,57 @@
 /*   By: lmohlala <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/05 07:17:53 by lmohlala          #+#    #+#             */
-/*   Updated: 2019/07/09 15:54:00 by xmethula         ###   ########.fr       */
+/*   Updated: 2019/07/09 16:57:46 by xmethula         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int			ft_readline(char **arrayh, char **line)
+static int			assline(char **arrstr, char **line)
 {
-	char			*temp;
+	char			*tmp;
 
-	if ((*arrayh)[0] == '\0')
+	if ((*arrstr)[0] == '\0')
 		return (0);
-	if (ft_strchr(*arrayh, '\n') != NULL)
+	if (ft_strchr(*arrstr, '\n') != NULL)
 	{
-		*(ft_strchr(*arrayh, '\n')) = '\0';
-		*line = ft_strdup(*arrayh);
-		temp = ft_strdup(ft_strchr(*arrayh, '\0') + 1);
-		free(*arrayh);
-		if (temp)
+		*(ft_strchr(*arrstr, '\n')) = '\0';
+		*line = ft_strdup(*arrstr);
+		tmp = ft_strdup(ft_strchr(*arrstr, '\0') + 1);
+		free(*arrstr);
+		if (tmp != NULL)
 		{
-			*arrayh = ft_strdup(temp);
-			free(temp);
+			*arrstr = ft_strdup(tmp);
+			free(tmp);
 		}
 	}
 	else
 	{
-		*line = ft_strdup(*arrayh);
-		ft_memdel((void **)arrayh);
+		*line = ft_strdup(*arrstr);
+		ft_strdel(arrstr);
 	}
 	return (1);
 }
 
 int					get_next_line(const int fd, char **line)
 {
-	int				rval;
+	static char		*str[1000];
 	char			buf[BUFF_SIZE + 1];
-	static char		*arr[1000];
-	char			*temp;
+	char			*tmp;
+	int				res;
 
-	if (fd < 0 || read(fd, buf, 0) < 0 || line == NULL)
+	if ((fd < 0) || (read(fd, buf, 0) < 0) || (line == NULL))
 		return (-1);
-	if (!(arr[fd]))
-		arr[fd] = ft_strnew(0);
-	while (!ft_strchr(arr[fd], '\n') && (rval = read(fd, buf, BUFF_SIZE)))
+	if (str[fd] == NULL)
+		str[fd] = ft_strnew(0);
+	while (!(ft_strchr(str[fd], '\n')) && (res = read(fd, buf, BUFF_SIZE)))
 	{
-		buf[rval] = '\0';
-		temp = ft_strjoin(arr[fd], buf);
-		free(arr[fd]);
-		arr[fd] = ft_strdup(temp);
-		free(temp);
+		tmp = ft_strjoin(str[fd], buf);
+		free(str[fd]);
+		str[fd] = ft_strdup(tmp);
+		free(tmp);
 	}
-	if (rval < 0)
+	if (res < 0)
 		return (-1);
-	return (ft_readline(&arr[fd], line));
+	return (assline(&str[fd], line));
 }
