cde525c1e97fb7564c9daaf0039fce848af277fe
x.r.methula@gmail.com <ray>
2019-07-12 21:44:51 +0200

    get_next_line.c file

--- get_next_line.c
+++ get_next_line.c
@@ -1,14 +1,4 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: lmohlala <marvin@42.fr>                    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/05 07:17:53 by lmohlala          #+#    #+#             */
-/*   Updated: 2019/07/12 09:06:19 by xmethula         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+/** 42 header **/
 
 #include "get_next_line.h"
 
@@ -18,14 +8,17 @@ static int			assignline(char **arr, char **line)
 
 	if ((*arr)[0] == '\0')
 		return (0);
-	if (ft_strchr(*arr, '\n'))
+	if (ft_strchr(*arr, '\n') != NULL)
 	{
 		*(ft_strchr(*arr, '\n')) = '\0';
 		*line = ft_strdup(*arr);
 		tmp = ft_strdup(ft_strchr(*arr, '\0') + 1);
 		free(*arr);
-		if (tmp != NULL)
-			*arr = tmp;
+		if (tmp)
+		{
+			*arr = ft_strdup(tmp);
+			free(tmp);
+		}
 	}
 	else
 	{
@@ -37,20 +30,22 @@ static int			assignline(char **arr, char **line)
 
 int					get_next_line(const int fd, char **line)
 {
-	static char		*arr[1000];
-	char			buf[BUFF_SIZE + 1];
-	char			*tmp;
-	int				res;
+	static char	*arr[999];
+	char		buf[BUFF_SIZE + 1];
+	char		*tmp;
+	int		res;
 
-	if ((fd < 0) || (read(fd, buf, 0) < 0) || (line == NULL))
+	if (fd < 0 || read(fd, buf, 0) < 0 || line == NULL)
 		return (-1);
-	if (arr[fd] == NULL)
+	if (!(arr[fd]))
 		arr[fd] = ft_strnew(0);
-	while (!(ft_strchr(arr[fd], '\n')) && (res = read(fd, buf, BUFF_SIZE)))
+	while (!ft_strchr(arr[fd], '\n') && (res = read(fd, buf, BUFF_SIZE)))
 	{
+		buf[res] = '\0';
 		tmp = ft_strjoin(arr[fd], buf);
 		free(arr[fd]);
-		arr[fd] = tmp;
+		arr[fd] = ft_strdup(tmp);
+		free(tmp);
 	}
 	if (res < 0)
 		return (-1);
